43b527ef24ac619b0a61fb046ede41b1dbf7ec58
akshat sahijpal <akshat.sahijpal@pw.live>
2023-10-11 02:32:14 +0530

    yuyakaido:cardstackview lib import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
+    implementation("com.github.yuyakaido:cardstackview:2.3.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
