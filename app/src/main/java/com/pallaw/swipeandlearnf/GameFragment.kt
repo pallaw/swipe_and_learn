bd89c83278a21a118bbb2ebc5ef0eae7286b7f48
Pallaw Pathak <pallaw.pathak@gmail.com>
2023-10-10 21:44:16 +0530

    basic setup

---
+++ app/src/main/java/com/pallaw/swipeandlearnf/GameFragment.kt
@@ -0,0 +1,62 @@
+package com.pallaw.swipeandlearnf
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.pallaw.swipeandlearnf.databinding.FragmentGameBinding
+
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class GameFragment : Fragment() {
+
+    private var _binding: FragmentGameBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_rewards -> {
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
