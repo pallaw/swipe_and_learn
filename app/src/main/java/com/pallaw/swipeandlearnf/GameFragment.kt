02d7135fb17a3d71d5a97eaf2727a3a628aaf9f9
akshat sahijpal <akshat.sahijpal@pw.live>
2023-10-11 14:22:58 +0530

    card swipe animation & functionality added

--- app/src/main/java/com/pallaw/swipeandlearnf/GameFragment.kt
+++ app/src/main/java/com/pallaw/swipeandlearnf/GameFragment.kt
@@ -7,17 +7,30 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DefaultItemAnimator
 import com.pallaw.swipeandlearnf.databinding.FragmentGameBinding
+import com.pallaw.swipeandlearnf.feature.adapter.QuestionsAdapter
+import com.pallaw.swipeandlearnf.feature.data.CardQuestionData
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.Direction
+import com.yuyakaido.android.cardstackview.SwipeableMethod
 
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class GameFragment : Fragment() {
+class GameFragment : Fragment(), CardStackListener, QuestionsAdapter.CardClickListener {
 
     private var _binding: FragmentGameBinding? = null
+    private val adapter = QuestionsAdapter(listOf(
+        CardQuestionData(),
+        CardQuestionData(),
+        CardQuestionData(),
+        CardQuestionData(),),this )
+
+    private lateinit var layoutManager: CardStackLayoutManager
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -38,6 +51,24 @@ class GameFragment : Fragment() {
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        layoutManager = CardStackLayoutManager(requireContext(), this).apply {
+            setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
+            setOverlayInterpolator(LinearInterpolator())
+        }
+
+        binding.stackView.layoutManager = layoutManager
+        binding.stackView.adapter = adapter
+        binding.stackView.itemAnimator.apply {
+            if (this is DefaultItemAnimator) {
+                supportsChangeAnimations = false
+            }
+        }
+
+
+    }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_main, menu)
         super.onCreateOptionsMenu(menu, inflater)
@@ -58,5 +89,26 @@ class GameFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCardDragging(direction: Direction?, ratio: Float) {
+    }
+
+    override fun onCardSwiped(direction: Direction?) {
+    }
+
+    override fun onCardRewound() {
+    }
+
+    override fun onCardCanceled() {
+    }
+
+    override fun onCardAppeared(view: View?, position: Int) {
+    }
+
+    override fun onCardDisappeared(view: View?, position: Int) {
+    }
+
+    override fun onVideoClicked(contentData: CardQuestionData?) {
     }
 }
