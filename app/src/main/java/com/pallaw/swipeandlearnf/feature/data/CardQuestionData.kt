c672f73e4b5e841bff5615a8d50818c9392442db
akshat sahijpal <akshat.sahijpal@pw.live>
2023-10-13 02:58:19 +0530

    CardQuestionData deleted

--- app/src/main/java/com/pallaw/swipeandlearnf/feature/data/CardQuestionData.kt
+++
@@ -1,7 +0,0 @@
-package com.pallaw.swipeandlearnf.feature.data
-
-data class CardQuestionData(
-    var question: String? = null,
-    var correctAnswer: Boolean? = null,
-    var hintsAvailable: String? = null,
-)
